50f8e21abe8c6c504186dfdc3e9cbbeac9cc63b6
YumaIshikawa18 <ishikawa_yuma@ims-sol.co.jp>
2025-01-19 22:31:38 +0900

    create package

---
+++ src/main/java/com/yuzukiku/dummy_reserve_log/DummyReserveLogApplication.java
@@ -0,0 +1,13 @@
+package com.yuzukiku.dummy_reserve_log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummyReserveLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DummyReserveLogApplication.class, args);
+	}
+
+}
